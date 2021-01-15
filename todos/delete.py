894e2666d6fd1fe2b9f85843b019a8f8e16710ae
EC2 Default User <ec2-user@ip-172-31-94-252.ec2.internal>
2021-01-15 21:53:54 +0000

    ahora si

--- todos/delete.py
+++ todos/delete.py
@@ -7,7 +7,7 @@ dynamodb = boto3.resource('dynamodb')
 def delete(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-    # delete the todo from the database 4
+    # delete the todo from the database 467
     table.delete_item(
         Key={
             'id': event['pathParameters']['id']
